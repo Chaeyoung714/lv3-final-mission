423c7e82d54747ad144e4d80c23af7fa4a42414e
Chaeyoung714 <202102760@hufs.ac.kr>
2025-06-10 14:15:41 +0900

    feat: 뮤지컬, 좌석등급, 시간, 예약 엔티티 관련 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO MEMBER (name, email, password)
 VALUES ('moda', 'moda@woowa.com', '1234');
+
+INSERT INTO MUSICAL (title, description)
+VALUES ('memphis', '내가 제일 좋아하는 공연!!');
+
+INSERT INTO RESERVATION (date, time, musical_id, member_id, seat_grade, seat_number)
+VALUES ('2025-06-30', 'EVENING', 1, 1, 'VIP', 10);
