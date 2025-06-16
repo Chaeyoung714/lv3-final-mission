d37b89656fd1c63cd014138b0d99df08429adea4
Chaeyoung714 <202102760@hufs.ac.kr>
2025-06-16 16:09:10 +0900

    feat: 공휴일 API 조회 기능 완성

--- src/main/java/finalmission/client/DataClient.java
+++ src/main/java/finalmission/client/DataClient.java
@@ -3,13 +3,10 @@ package finalmission.client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import finalmission.client.dto.HolidaysResponse;
 import java.net.URI;
-import java.util.Map;
-import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriBuilder;
 
 @Component
 public class DataClient {
@@ -30,24 +27,19 @@ public class DataClient {
 
     public HolidaysResponse getHolidayData(int year, int month) {
         String monthMessage = buildMonthMessage(month);
-        Map<String, String> queryParams = Map.of(
-                "serviceKey", SERVICE_KEY,
-                "solYear", year + "",
-                "solMonth", monthMessage,
-                "_type", "json"
-        );
 
-        Function<UriBuilder, URI> uriBuilderURIFunction = uriBuilder ->
-//                uriBuilder.path(DATA_API_URI + "/getRestDeInfo")
-                uriBuilder.scheme("https").host("apis.data.go.kr")
-                        .path("/B090041/openapi/service/SpcdeInfoService/getRestDeInfo")
-                        .queryParam("serviceKey", SERVICE_KEY)
-                        .queryParam("solYear", year + "")
-                        .queryParam("solMonth", monthMessage)
-                        .queryParam("_type", "json")
-                        .build();
+        StringBuilder uri = new StringBuilder(DATA_API_URI + "/getRestDeInfo");
+        uri.append("?serviceKey=");
+        uri.append(SERVICE_KEY);
+        uri.deleteCharAt(uri.lastIndexOf("}"));
+        uri.append("&solYear=");
+        uri.append(year + "");
+        uri.append("&solMonth=");
+        uri.append(monthMessage);
+        uri.append("&_type=json");
+
         return restClient.get()
-                .uri(uriBuilderURIFunction)
+                .uri(URI.create(uri.toString()))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .toEntity(HolidaysResponse.class)
--- src/main/java/finalmission/client/dto/HolidaysResponse.java
+++ src/main/java/finalmission/client/dto/HolidaysResponse.java
@@ -5,21 +5,37 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public record HolidaysResponse(
-        List<HolidayItem> items,
-        int numOfRows,
-        int pageNo,
-        int totalCount
+        HolidayResponse response
 ) {
-    private static final DateTimeFormatter holidayDateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-    public List<LocalDate> getHolidayDates() {
-        return this.items().stream()
-                .map(item -> LocalDate.parse(item.locdate(), holidayDateFormatter))
-                .toList();
-    }
+        private static final DateTimeFormatter holidayFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-    public record HolidayItem (
-        String locdate
-    ) {
-    }
+        public List<LocalDate> getHolidayDates() {
+                return response.body().items().item().stream()
+                        .map(holidayItem -> LocalDate.parse(holidayItem.locdate(), holidayFormatter))
+                        .toList();
+        }
+
+        public record HolidayResponse(
+                HolidayBody body
+        ) {
+        }
+
+        public record HolidayBody (
+                HolidayItems items,
+                int numOfRows,
+                int pageNo,
+                int totalCount
+        ) {
+        }
+
+        public record HolidayItems (
+                List<HolidayItem> item
+        ) {
+        }
+
+        public record HolidayItem (
+                String locdate
+        ) {
+        }
 }
